5018d8ce0a7e20441dff265d96fca0f2a9b24f0d
zaoansijia <410860465@qq.com>
2020-10-11 23:44:42 +0800

    promise 完善加验证setState是同步还是异步

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React from 'react';
+// import ReactDOM from 'react-dom';
 import './App.css';
 import './code/promise.js';
+class App extends React.Component{
 
-const App = () => {
-  return (
-    <div>
-      <h1>常见的手写代码</h1>
-      <ul>
-        <li>手写promise</li>
-      </ul>
-    </div>
-  )
+  constructor() {
+    super();
+    this.state =  {
+      count: 0
+    }
+  }
+  componentDidMount() {
+    // 测试原生方法：手动绑定mousedown事件
+    // ReactDOM.findDOMNode(this).addEventListener(
+    //   "mousedown",
+    //   this.click
+    // );
+  }
+  asyncClick = async () => {
+    console.log('async-pre-click',this.state.count);
+    await this.setState({count:this.state.count+1});
+    console.log('async-next-click',this.state.count);
+  }
+  click = () => {
+    console.log('click-pre-count',this.state.count);
+    this.asyncClick();
+    console.log('click-next-count',this.state.count);
+  }
+  render() {
+    return (
+      <div>
+        <h1>常见的手写代码</h1>
+        <ul>
+          <li>手写promise</li>
+          <li onClick={this.click}>点击测试一下setState是同步还是异步</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/code/promise.js
+++ src/code/promise.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-loop-func */
 
 // 手写promise；
 class Promise {
@@ -14,17 +15,14 @@ class Promise {
     // 调用此函数表示成功
     const resolve = (data) => {
       if (this.status !== 'pending') return;
-      // console.log('resolve-----',data);
       this.status = 'fulfilled';
       setTimeout(() => {
         this.resoveList.forEach(s => {
-          console.log('foreach------',data, typeof s);
-          // data = s(data);
-
-          if((typeof s==='object'&&s !== null) || typeof s === 'function') {
+          // then里面如果包含的是非函数，直接值穿透
+          if ((typeof s === 'object' && s !== null) || typeof s === 'function') {
             data = s(data);
-           // 从新赋值data的值继承上个resolve的返回值
-          }else {
+            // 从新赋值data的值继承上个resolve的返回值
+          } else {
             data = s;
           }
         })
@@ -35,9 +33,9 @@ class Promise {
       if (this.status !== 'pending') return;
       this.status = 'rejected';
       setTimeout(() => {
-        this.rejectList.forEach(f => { 
+        this.rejectList.forEach(f => {
           // 从新赋值err的值继承上个reject的返回值
-          err = f(err) 
+          err = f(err)
         });
       })
     }
@@ -49,108 +47,104 @@ class Promise {
       reject(error)
     }
   }
-  then(scb, fcb) {
-    if (scb) {
-      this.resoveList.push(scb);
+  // then后面跟两个函数，完成函数，和错误拒绝函数
+  then(onResolved, onRejected) {
+    if (onResolved) {
+      this.resoveList.push(onResolved);
     }
-    if (fcb) {
-      this.rejectList.push(fcb);
+    if (onRejected) {
+      this.rejectList.push(onRejected);
     }
-    console.log(this, 'this------')
     return this;
   }
-  catch(cb) {
-    if (cb) {
-      this.rejectList.push(cb);
+  // catch 后面跟错误处理函数
+  catch(onRejected) {
+    if (onRejected) {
+      this.rejectList.push(onRejected);
     }
     return this;
   }
 
-  //   /**
-  //    * 实现Promise.resolve
-  //    * 1.参数是一个 Promise 实例, 那么Promise.resolve将不做任何修改、原封不动地返回这个实例。
-  //    * 2.如果参数是一个原始值，或者是一个不具有then方法的对象，则Promise.resolve方法返回一个新的 Promise 对象，状态为resolved。
-  //   */
-  //   static resolve(data) {
-  //     if (data instanceof Promise) {
-  //       return data;
-  //     } else {
-  //       return new Promise((resolve, reject) => {
-  //         resolve(data);
-  //       })
-  //     }
-  //   }
-  //   // 实现Promise.reject
-  //   static reject(err) {
-  //     if (err instanceof Promise) {
-  //       return err;
-  //     } else {
-  //       return new Promise((resolve, reject) => {
-  //         reject(err);
-  //       })
-  //     }
-  //   }
-  //   /**
-  //    * 实现Promise.all
-  //    * 1. Promise.all方法接受一个数组作为参数，p1、p2、p3都是 Promise 实例，如果不是，就会先调用下面讲到的Promise.resolve方法，将参数转为 Promise 实例，再进一步处理。
-  //    * 2. 返回值组成一个数组
-  //   */
-  //   static all(promises) {
-  //     return new Promise((resolve, reject) => {
-  //       let promiseCount = 0;
-  //       let promisesLength = promises.length;
-  //       let result = [];
-  //       for (let i = 0; i < promises.length; i++) {
-  //         // promises[i]可能不是Promise类型，可能不存在then方法，中间如果出错,直接返回错误
-  //         Promise.resolve(promises[i])
-  //           .then(res => {
-  //             promiseCount++;
-  //             // 注意这是赋值应该用下标去赋值而不是用push，因为毕竟是异步的，哪个promise先完成还不一定
-  //             result[i] = res;
-  //             if (promiseCount === promisesLength) {
-  //               return resolve(result);
-  //             }
-  //           }, (err) => {
-  //             return reject(err);
-  //           }
-  //           )
-  //       }
-  //     })
-  //   }
-  //   /**
-  //    * 实现Promise.race
-  //    * 1. Promise.race方法的参数与Promise.all方法一样，如果不是 Promise 实例，就会先调用下面讲到的Promise.resolve方法，将参数转为 Promise 实例，再进一步处理。
-  //    * 2. 返回那个率先改变的 Promise 实例的返回值
-  //   */
-  //   static race(promises) {
-  //     return new Promise((resolve, reject) => {
-  //       for (let i = 0; i < promises.length; i++) {
-  //         Promise.resolve(promises[i])
-  //           .then(res => {
-  //             return resolve(res);
-  //           }, (err) => {
-  //             return reject(err);
-  //           }
-  //           )
-  //       }
-  //     })
-  //   }
+    /**
+     * 实现Promise.resolve
+     * 1.参数是一个 Promise 实例, 那么Promise.resolve将不做任何修改、原封不动地返回这个实例。
+     * 2.如果参数是一个原始值，或者是一个不具有then方法的对象，则Promise.resolve方法返回一个新的 Promise 对象，状态为resolved。
+    */
+    static resolve(data) {
+      if (data instanceof Promise) {
+        return data;
+      }
+      return new Promise((resolve) => {
+        resolve(data);
+      })
+    }
+    // 实现Promise.reject
+    static reject(err) {
+      if (err instanceof Promise) {
+        return err;
+      }
+      return new Promise((resolve, reject) => {
+        reject(err);
+      })
+    }
+    /**
+     * 实现Promise.all
+     * 1. Promise.all方法接受一个数组作为参数，p1、p2、p3都是 Promise 实例，如果不是，就会先调用下面讲到的Promise.resolve方法，将参数转为 Promise 实例，再进一步处理。
+     * 2. 返回值组成一个数组
+    */
+    static all(promises) {
+      return new Promise((resolve, reject) => {
+        let promiseCount = 0;
+        let result = [];
+        for (let i = 0; i < promises.length; i++) {
+          // promises[i]可能不是Promise类型，可能不存在then方法，中间如果出错,直接返回错误
+          Promise.resolve(promises[i])
+            .then(res => {
+              promiseCount++;
+              // 注意这里赋值应该用下标去赋值而不是用push，因为毕竟是异步的，哪个promise先完成还不一定
+              result[i] = res;
+              if (promiseCount === promises.length) {
+                return resolve(result);
+              }
+            }, reject
+            )
+        }
+      })
+    }
+    /**
+     * 实现Promise.race
+     * 1. Promise.race方法的参数与Promise.all方法一样，如果不是 Promise 实例，就会先调用下面讲到的Promise.resolve方法，将参数转为 Promise 实例，再进一步处理。
+     * 2. 返回那个率先改变的 Promise 实例的返回值
+    */
+    static race(promises) {
+      return new Promise((resolve, reject) => {
+        for (let i = 0; i < promises.length; i++) {
+          Promise.resolve(promises[i])
+            .then(resolve, reject);
+        }
+      })
+    }
 }
 
-const p = new Promise((resolve, reject) => {
+const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log('------手写promise-----');
-    resolve('成功了');
-
+    resolve('成功了1');
   }, 1000)
 })
-// p.resolve(2);
-p.then('成功了2').then(data2 => {
-  console.log('data2', data2);
-  return '成功了3'
-}).then(data3 => {
-  console.log('data3', data3);
-}).catch(err => {
-  console.log('error', err);
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('成功了2');
+
+  }, 500)
 })
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('成功了3');
+  }, 0)
+})
+Promise.race([p1,p2,p3]).then((res)=>{
+  console.log(res,'res');
+})
+
 
